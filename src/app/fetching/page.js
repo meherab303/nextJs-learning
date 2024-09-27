9393021d3c0fef18a5c7452035d9cee0290cabe2
meherab nahin <nahinrahman87@gmail.com>
2024-09-28 00:52:39 +0600

    revalidate data

--- src/app/fetching/page.js
+++ src/app/fetching/page.js
@@ -1,12 +1,16 @@
 import Image from 'next/image';
 import React from 'react';
-
 const NextJsDataFetching = async() => {
-    const response=await fetch('http://localhost:5000/shoes')
+    const response=await fetch('http://localhost:5000/shoes',{
+        next:{
+            revalidate:5
+        }
+    })
     const shoes=await response.json()
     
     return (
         <div className='grid grid-cols-3 gap-8 m-8'>
+            
             {
                 shoes?.map((shoe)=>(
                 <div key={shoe.id} className="card bg-base-100 w-96 shadow-xl">
@@ -39,5 +43,6 @@ export default NextJsDataFetching;
 /*
 1.nextJs e fetching er jnno must server component hote hbe.client component e fetch kaj korbena
 2.must asynce component hote hbe fetching er jnno
+3.{cache:'force-cache'} dile content gula cache hoye thakbe and build korar shomoi static content er html file crate kore rakhbe.ei property na dile bydefault e hoye jabe.
 
 */
