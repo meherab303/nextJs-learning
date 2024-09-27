06e8e979f6935b3bdb1a0fe9bbf19dc14534bb0a
meherab nahin <nahinrahman87@gmail.com>
2024-09-27 21:09:34 +0600

    next level data fetching of shoes

---
+++ src/__fundamentals__.js
@@ -0,0 +1,12 @@
+/*
+1.automatic folder er name onojayi route create hoye jai.so folder name small letter e hobe.
+2.routing er jnno obosshoi app folder er modde folder create kora lghbe .And (page.js) file must thaka lghbei.layout change korte chaile (layout.js) file create krte hbe.
+3.dynamic routing er jnno parent folder er modde  [folder name] ei structer e folder create korte hbe.tahole localhost:3000/about/folder name -->onyjayi routing hbe
+like:localhost:3000/about/1,localhost:3000/about/2
+4.catchAllRoutes: localhost:3000/about/2/3/4/56/ ei routing er jnnno parent folder er modde[...folder name] eivabe create korte hbe.tahole parent folder por / diye ja deya hbe tai routing hoye jbae
+5.group routing er jnnno (anyName)/folder name ei structer e create korte hbe
+6.<Link href=''>
+7.css er jnno--> anyName.module.css
+8.seo friendly er jnno metada use korte hbe
+
+*/
---
+++ src/app/fetching/page.js
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import React from 'react';
+
+const NextJsDataFetching = async() => {
+    const response=await fetch('http://localhost:5000/shoes')
+    const shoes=await response.json()
+    
+    return (
+        <div className='grid grid-cols-3 gap-8 m-8'>
+            {
+                shoes?.map((shoe)=>(
+                <div key={shoe.id} className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                      <Image
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" height={400} width={400} />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">
+                      {shoe.title}
+                        <div className="badge badge-secondary">NEW</div>
+                      </h2>
+                      <p>{shoe.description}</p>
+                      <div className="card-actions justify-end">
+                        <div className="badge badge-outline">{shoe.category}</div>
+                        <div className="badge badge-outline">{shoe.price}$</div>
+                      </div>
+                    </div>
+                  </div>
+                    
+                ))
+            }
+        </div>
+    );
+};
+
+export default NextJsDataFetching;
+
+/*
+1.nextJs e fetching er jnno must server component hote hbe.client component e fetch kaj korbena
+2.must asynce component hote hbe fetching er jnno
+
+*/
